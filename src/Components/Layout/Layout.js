54361396b7f72c7e87966db8e87db4f89bdd3b44
Akash Kumar <aksh.iem22@gmail.com>
2020-05-03 21:41:04 +0530

    responsive

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import Aux from '../../hoc/Aux';
-
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.css';
 
 const layout = ( props ) => (
     <Aux>
         <Toolbar />
+        <SideDrawer />
         <main className={classes.Content}>
             {props.children}
         </main>
